2a5a49dcbf0a2624fecfef4fa6f3105d54af2a0f
Chloe Horgan <super.chloe@gmail.com>
2016-01-05 20:08:10 -0500

    Add image snap into place

--- Photo Bombers/DetailViewController.m
+++ Photo Bombers/DetailViewController.m
@@ -12,6 +12,7 @@
 @interface DetailViewController ()
 
 @property (nonatomic) UIImageView *imageView;
+@property (nonatomic) UIDynamicAnimator *animator;
 
 @end
 
@@ -22,7 +23,7 @@
     
     self.view.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.95];
     
-    self.imageView = [[UIImageView alloc] init];
+    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, -320.0, 320.0f, 320.0f)];
     [self.view addSubview:self.imageView];
     
     [PhotoController imageForPhoto:self.photo size:@"standard_resolution" completion:^(UIImage *image) {
@@ -32,16 +33,13 @@
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(close)];
     [self.view addGestureRecognizer:tap];
     
+    self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    
+    UISnapBehavior *snap = [[UISnapBehavior alloc] initWithItem:self.imageView snapToPoint:self.view.center];
+    [self.animator addBehavior:snap];
+    
 }
 
-- (void)viewDidLayoutSubviews {
-    [super viewDidLayoutSubviews];
-    
-    CGSize size = self.view.bounds.size;
-    CGSize imageSize = CGSizeMake(size.width, size.width);
-    
-    self.imageView.frame = CGRectMake(0.0, (size.height - imageSize.height) / 2.0, imageSize.width, imageSize.height);
-}
 
 - (void)close {
     [self dismissViewControllerAnimated:YES completion:nil];
